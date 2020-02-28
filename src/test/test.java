a5d7f140ccff8a5d3271fe2ab6d55ba9462d0ee7
Hiba Ghanim <hibaghanim5@gmail.com>
2020-02-28 21:08:50 +0200

    first commit

---
+++ src/test/test.java
@@ -0,0 +1,51 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import myCalculator.Calculator;
+
+public class test {
+
+	@Test
+	public void testSum_BothNumbersArePositive_ShouldReturnPositiveNumber()
+	{
+		Calculator c=new Calculator();
+		int result=c.add(2,3);
+		assertTrue(result>0);
+	}
+	
+	@Test
+	public void testSum_BothNumbersAreNegative_ShouldReturnNegativeNumber()
+	{
+		Calculator c=new Calculator();
+		int result=c.add(-2,-3);
+		assertTrue(result<0);
+	}
+	
+
+	@Test
+	public void testSum_OneNumberIsNegativeAndOtherIsPositiveAndTheyAreEqual_ShouldReturnZero()
+	{
+		Calculator c=new Calculator();
+		int result=c.add(-2,2);
+		assertTrue(result==0);
+	}
+	
+	@Test
+	public void testSum_OneNumberIsNegativeAndOtherIsPositiveAndPositiveIsBigger_ShouldReturnPositive()
+	{
+		Calculator c=new Calculator();
+		int result=c.add(-2,3);
+		assertTrue(result>0);
+	}
+	
+	@Test
+	public void testSum_OneNumberIsNegativeAndOtherIsPositiveAndNegativeIsBigger_ShouldReturnNegative()
+	{
+		Calculator c=new Calculator();
+		int result=c.add(2,-3);
+		assertTrue(result<0);
+	}
+}
